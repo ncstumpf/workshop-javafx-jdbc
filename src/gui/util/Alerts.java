7f1adbe2d8e192e0748534ab2b049b901bee16d0
Nicolas Stumpf <ncs.stumpf@gmail.com>
2020-07-15 21:27:05 -0300

    About view

---
+++ src/gui/util/Alerts.java
@@ -0,0 +1,16 @@
+package gui.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Alerts {
+	
+	public static void ShowAlert (String title, String head, String content, AlertType type) {
+		Alert alert = new Alert (type);
+		alert.setTitle(title);
+		alert.setHeaderText(head);
+		alert.setContentText(content);
+		alert.show();		
+	}
+
+}
